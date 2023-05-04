bac2f136d43a2d8abe8a09dd7c050cd1408ad088
rjmfernandes <rjmfernandes@gmail.com>
2023-05-04 12:06:42 +0200

    example file upload inspection

--- ContentInspectionExampleAppApp.ts
+++ ContentInspectionExampleAppApp.ts
@@ -19,9 +19,15 @@ export class ContentInspectionExampleAppApp extends App implements IPreFileUploa
     async [AppMethod.EXECUTE_PRE_FILE_UPLOAD](context: IFileUploadContext, read: IRead, http: IHttp, persis: IPersistence, modify: IModify): Promise<void> {
         const user = await read.getUserReader().getById(context.file.userId);
         const room = await read.getRoomReader().getById(context.file.rid);
-        console.log('ContentInspectionExampleAppApp - File Uploaded - Name: '+context.file.name);
-        console.log('ContentInspectionExampleAppApp - File Uploaded - Type: '+context.file.type);
-        console.log('ContentInspectionExampleAppApp - File Uploaded - Size: '+context.file.size);
+        console.log('ContentInspectionExampleAppApp - File Uploaded - Name: ' + context.file.name);
+        console.log('ContentInspectionExampleAppApp - File Uploaded - Type: ' + context.file.type);
+        console.log('ContentInspectionExampleAppApp - File Uploaded - Size: ' + context.file.size);
+
+        if (context.file.type == 'text/plain') {
+            console.log('ContentInspectionExampleAppApp - File Uploaded - Content: ' +
+                String.fromCharCode.apply(null, context.content));
+        }
+
         //if file is bad
         //throw new FileUploadNotAllowedException('File is Bad');
     }
