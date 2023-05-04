bbe196a876e07397c41727a2bb72bc7c192f4a1d
rjmfernandes <rjmfernandes@gmail.com>
2023-05-04 12:36:03 +0200

    added message inspection

--- ContentInspectionExampleAppApp.ts
+++ ContentInspectionExampleAppApp.ts
@@ -7,28 +7,57 @@ import {
     IRead,
 } from '@rocket.chat/apps-engine/definition/accessors';
 import { App } from '@rocket.chat/apps-engine/definition/App';
+import { IMessage } from '@rocket.chat/apps-engine/definition/messages';
+import { IPreMessageSentPrevent } from '@rocket.chat/apps-engine/definition/messages/IPreMessageSentPrevent';
 import { AppMethod, IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { IRoom } from '@rocket.chat/apps-engine/definition/rooms/IRoom';
 import { IFileUploadContext } from '@rocket.chat/apps-engine/definition/uploads';
 import { IPreFileUpload } from '@rocket.chat/apps-engine/definition/uploads/IPreFileUpload';
+import { IUser } from '@rocket.chat/apps-engine/definition/users/IUser';
 //import { FileUploadNotAllowedException } from '@rocket.chat/apps-engine/definition/exceptions';
 
-export class ContentInspectionExampleAppApp extends App implements IPreFileUpload {
+export class ContentInspectionExampleAppApp extends App implements IPreFileUpload, IPreMessageSentPrevent {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
     }
+    async checkPreMessageSentPrevent?(message: IMessage, read: IRead, http: IHttp): Promise<boolean> {
+        //will only check in case the room is not general
+        return (message.room.slugifiedName !== 'general');
+    }
+    async executePreMessageSentPrevent(message: IMessage, read: IRead, http: IHttp, persistence: IPersistence): Promise<boolean> {
+        //if message is 'test' wont allow
+        if (message.text === 'test' && message.room) {
+            this.notifyMessage(message.room, read, message.sender, 'Message is not allowed');
+            return true;
+        }
+        return false;
+    }
+
+    private async notifyMessage(room: IRoom, read: IRead, sender: IUser, message: string): Promise<void> {
+        const notifier = read.getNotifier();
+        const messageBuilder = notifier.getMessageBuilder();
+        messageBuilder.setText(message);
+        messageBuilder.setRoom(room);
+        await notifier.notifyUser(sender, messageBuilder.getMessage());
+    }
+
     async [AppMethod.EXECUTE_PRE_FILE_UPLOAD](context: IFileUploadContext, read: IRead, http: IHttp, persis: IPersistence, modify: IModify): Promise<void> {
         const user = await read.getUserReader().getById(context.file.userId);
-        const room = await read.getRoomReader().getById(context.file.rid);
         console.log('ContentInspectionExampleAppApp - File Uploaded - Name: ' + context.file.name);
         console.log('ContentInspectionExampleAppApp - File Uploaded - Type: ' + context.file.type);
         console.log('ContentInspectionExampleAppApp - File Uploaded - Size: ' + context.file.size);
 
-        if (context.file.type == 'text/plain') {
+        if (context.file.type === 'text/plain') {
             console.log('ContentInspectionExampleAppApp - File Uploaded - Content: ' +
                 String.fromCharCode.apply(null, context.content));
         }
 
         //if file is bad
         //throw new FileUploadNotAllowedException('File is Bad');
+
+        const room = await read.getRoomReader().getById(context.file.rid);
+        if (room) {
+            this.notifyMessage(room, read, user, 'File inspected - Check logs');
+        }
     }
 }
